9ae7345dabc9aaafedcaac021bd7d84757671b52
robogeek95 <azeezlukman95@gmail.com>
2021-12-02 01:30:08 +0100

    added components

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="py-6 bg-black text-white text-center">
+      <p>© 2021 Octo Store. All rights reserved</p>
+    </div>
+  );
+}
---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="bg-white relative border-b-2">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-center items-center text-center border-gray-100 py-6 md:space-x-10">
+          <p className="text-3xl">Octo Store</p>
+        </div>
+      </div>
+    </div>
+  );
+}
