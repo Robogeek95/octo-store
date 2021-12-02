1e85789486ffa0ded104006b0c1f2b1654204c70
robogeek95 <azeezlukman95@gmail.com>
2021-12-02 01:48:41 +0100

    base home page

--- components/navbar.js
+++ components/navbar.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="bg-white relative border-b-2">
+    <div className="bg-white relative border-b-2 border-gray-100 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
-        <div className="flex justify-center items-center text-center border-gray-100 py-6 md:space-x-10">
+        <div className="flex justify-center items-center text-centerpy-6 md:space-x-10">
           <p className="text-3xl">Octo Store</p>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,41 @@
-import Head from 'next/head'
-import Footer from '../components/footer'
-import Navbar from '../components/navbar'
+import Head from "next/head";
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <main>
-        main here
+      <main className="max-w-7xl mx-auto">
+        <div className="grid grid-col-2 my-40">
+          <div>
+            <p className="text-6xl">Hey there,</p>
+            <p className="text-6xl font-bold mt-2">Let's text</p>
+            <p className="mt-8">
+              Signup below to get exciting deals and the latest menu right on
+              your phone. We'll send you a welcome offer just for opting in
+            </p>
+
+            <form className="mt-4">
+              <div>
+                <input
+                  className="p-2 border-2 border-gray-400"
+                  placeholder="Mobile number"
+                  type="text"
+                />
+              </div>
+
+              <div className="mt-4">
+                <button className="bg-purple-600 py-3 text-white px-6">
+                  Sign me up
+                </button>
+              </div>
+            </form>
+          </div>
+          <div>hi</div>
+        </div>
       </main>
       <Footer />
     </div>
-  )
+  );
 }
